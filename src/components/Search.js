fcbd0bf67686d873ea833b8d1e87e1c6a9c089f1
Yuvalkop22 <Kopelman050@gmail.com>
2022-11-17 19:36:26 +0200

    Update Search.js
    change

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,36 +1,27 @@
 import React from 'react';
 
 const Search = (props) => {
-    this.state = {
-        items: [],
-        DataisLoaded: false
-    };
-    const execute = () => {
-        fetch("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=AIzaSyAHsKpoDCFfLdMVct7VhEqDCoUrqqXuYaM").
-            then((res) => res.json()).then((json) => {
-                this.state({
-                    items: json,
-                    DataisLoaded: true
-                });
-            });
-    };
-    return (
+    // this.state = {
+    //     items: [],
+    //     DataisLoaded: false
+    // };
+    // const execute = () => {
+    //     fetch("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=AIzaSyAHsKpoDCFfLdMVct7VhEqDCoUrqqXuYaM").
+    //         then((res) => res.json()).then((json) => {
+    //             this.state({
+    //                 items: json,
+    //                 DataisLoaded: true
+    //             });
+    //         });
+    // };
+     return (
         <div>
             <h1>Hello , {props.value}</h1>
             <input type={'text'} />
             <button>Click me to search</button>
             <h1> Fetch data from an api in react </h1>
-            {
-                items.map((item) => (
-                    <ol key={item.id} >
-                        User_Name: {item.username},
-                        Full_Name: {item.name},
-                        User_Email: {item.email}
-                    </ol>
-                ))
-            }
         </div>
-    );
+     )
 };
 
 export default Search;
