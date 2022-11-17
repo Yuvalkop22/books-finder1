1d6343e95a5beb0306712c35caa3e31cc75c69ad
Yuvalkop22 <Kopelman050@gmail.com>
2022-11-17 19:31:55 +0200

    Update Search.js
    stam

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,17 +5,32 @@ const Search = (props) => {
         items: [],
         DataisLoaded: false
     };
-    const execute = () =>{
-        fetch("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=AIzaSyAHsKpoDCFfLdMVct7VhEqDCoUrqqXuYaM")
-    }
-    return ( 
-    <div>
-        <h1>Hello , {props.value}</h1>
-        <input type={'text'}/>
-        <button>Click me to search</button>
-        
-    </div>
+    const execute = () => {
+        fetch("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=AIzaSyAHsKpoDCFfLdMVct7VhEqDCoUrqqXuYaM").
+            then((res) => res.json()).then((json) => {
+                this.state({
+                    items: json,
+                    DataisLoaded: true
+                });
+            });
+    };
+    return (
+        <div>
+            <h1>Hello , {props.value}</h1>
+            <input type={'text'} />
+            <button>Click me to search</button>
+            <h1> Fetch data from an api in react </h1>
+            {
+                items.map((item) => (
+                    <ol key={item.id} >
+                        User_Name: {item.username},
+                        Full_Name: {item.name},
+                        User_Email: {item.email}
+                    </ol>
+                ))
+            }
+        </div>
     );
-}
- 
+};
+
 export default Search;
