c0d378bbfe6af23beebd1bcdb952fb5eaf5d3f5e
Yuvalkop22 <Kopelman050@gmail.com>
2022-11-18 02:48:00 +0200

    donefortoday

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,22 @@
-import {React, useState} from 'react';
+import {React, useState, useRef} from 'react';
 import BookList from './BookList';
 const Search = (props) => {
     const [all,setAll] = useState([]);
     const [isShown, setIsShown] = useState(false);
+    const [searchValue,setSearchValue] = useState("");
+    const searchRef = useRef();
     const fetching = (e) => {
         fetch("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=AIzaSyAHsKpoDCFfLdMVct7VhEqDCoUrqqXuYaM")
             .then((res) => res.json())
             .then((data) => setAll(data.items));
+        setSearchValue(searchRef.current.value);
+        console.log(searchValue);
         //setIsShown(current => !current);
     }
     return (
         <div>
             <h1>Hello , {props.value}</h1>
-            <input type={'text'} />
+            <input type={'text'} ref={searchRef} placeholder={"Enter Author Name"} />
             <button onClick={fetching}>Click Me to Search</button>
             {/* {
                 isShown && (
@@ -21,7 +25,9 @@ const Search = (props) => {
                     </div>
                 )
             } */}
-            {all.map(item => <h1>{item.volumeInfo.title}</h1>, console.log(all))}            
+            {
+                all.filter(item => item.id === searchValue).map(item => <h1>{item.volumeInfo.title}</h1>)            
+            }            
             
             
         </div>
